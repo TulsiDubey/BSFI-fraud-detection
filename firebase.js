bcf4eca57404ca4d0ef14c09bc78fbda5de6191b
TulsiDubey <dubeytulsi3@gmail.com>
2025-06-27 21:18:11 +0530

    firebase

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2QdLSfYX587yackaTTUtx_D_MLoLcvS4",
+    authDomain: "bfsi-fraud-detection-app.firebaseapp.com",
+    projectId: "bfsi-fraud-detection-app",
+    storageBucket: "bfsi-fraud-detection-app.firebasestorage.app",
+    messagingSenderId: "73965566370",
+    appId: "1:73965566370:web:7326e3529b30a08cde5afb",
+    measurementId: "G-HJ3HSCNXMN"
+  };
